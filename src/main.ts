92071577e1bda04c86bad470db8fe60411d4a31b
gaoguanqi <307590625@qq.com>
2021-01-30 14:49:39 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import globalComponents from '@/components/index'
+import '@/assets/icon'
+import '@/assets/style/sass/index.scss'
+import 'element-plus/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+app
+  .use(store)
+  .use(router)
+  .use(ElementPlus)
+  .use(globalComponents)
+  .mount('#app')
---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>home</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
