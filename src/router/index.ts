b41091a91485b9c99bd533df465bdae846b40736
gaoguanqi <307590625@qq.com>
2021-01-30 16:09:30 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,43 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: Array<RouteRecordRaw> = [
+const _component = (file: string) => import(`@/views/${file}/index.vue`)
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    name: 'login',
+    meta: {
+      hidden: true,
+      title: '登录'
+    },
+    component: _component('login')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    meta: {
+      hidden: true,
+      title: '首页'
+    },
+    component: _component('home')
+  },
+  {
+    path: '/404',
+    name: '404',
+    meta: {
+      hidden: true,
+      title: '错误'
+    },
+    component: _component('404')
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
